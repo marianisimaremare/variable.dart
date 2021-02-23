d92f75506af3acfd1521a32d091d1ae33030404b
marianisimaremare <79125982+marianisimaremare@users.noreply.github.com>
2021-02-23 12:59:02 +0700

    Update and rename README.md to variable.dart

---
+++ variable.dart
@@ -0,0 +1,13 @@
++ variable.dart
++ void main() {
++   String nama = "Mariana Simaremare";
++   int umur = 21;
++   double tinggi = 150.35;
++   bool isMenikah = false;
++
++   var alamat = "Siborong-borong, Indonesia";
++
++   print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
++   print("Menikah: $isMenikah");
++   print("Alamat: $alamat");
++   }
